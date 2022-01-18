78fadee4da1a1063fee6f6888b8b0e1f40a2df95
MarcusMendes81 <maarcusmeendes@gmail.com>
2022-01-18 10:19:37 -0300

    Criando classe Conta com seus atributos e suas funções.

---
+++ Projetos/src/one/dio/digionebank/Conta.kt
@@ -0,0 +1,17 @@
+package one.dio.digionebank
+
+import java.math.BigDecimal
+
+class Conta(
+    val agencia: String,
+    val conta: String,
+    val saldo: BigDecimal
+
+) {
+    fun desposito(valor: BigDecimal){
+
+    }
+    fun saque(valor: BigDecimal){
+
+    }
+}
